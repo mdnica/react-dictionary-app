7d3605b37d147df4dcfcb84a0c14a39303fc464f
diana nica <themindhelp@gmail.com>
2023-02-10 15:56:33 +0000

    created Search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
+import "./Dictionary.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a href="#" className="btn btn-primary">
-          Hello there
-        </a>
-      </header>
+      <div className="container-fluid">Hello</div>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>Coded by Di</footer>
     </div>
   );
 }
